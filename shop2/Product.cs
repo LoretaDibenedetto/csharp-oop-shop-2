a8293f6a11108b341e62784e88b4fc00c38c3cb7
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-20 14:53:05 +0200

    Add project files.

---
+++ shop2/Product.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+
+
+namespace shop
+{
+    public class Product: shop
+    {
+        private string nameProduct;
+        private string descriptionProduct;
+        private int barCode;
+        private bool onSale;
+        private float price;
+
+        //Costruttore
+        public Product(string nameProduct, string descriptionProduct, bool onSale, float price, string shopName, string shopCity, string shopAddress, int shopCivicNUmber) : base(shopName, shopCity, shopAddress, shopCivicNUmber)
+        {
+
+            this.nameProduct = nameProduct;
+            this.descriptionProduct = descriptionProduct;
+            this.barCode = GenerateCode();
+            this.onSale = onSale;
+            this.price = price;
+
+
+        }
+
+        //Getters setters
+
+        //nome prodotto
+        public string NameProduct
+        {
+            get { return this.nameProduct; }
+            set { this.nameProduct = value; }
+        }
+
+        //descrizione
+        public string DescriptionProduct
+        {
+            get { return this.descriptionProduct; }
+            set { this.descriptionProduct = value; }
+        }
+        //codice a barre
+        public int BarCode
+        {
+            get { return this.barCode; }
+        }
+        //prezzo prodotto
+        public float Price
+        {
+            get { return (float)this.price; }
+            set { this.price = (float)value; }
+        }
+
+       
+
+        //saldo prodotto 
+        //getters
+        public bool GetOnSale()
+        {
+            return this.onSale;
+        }
+
+        //setters
+        public bool SetOnSale()
+        {
+            return onSale = false;
+        }
+
+
+        //metodi
+
+        //generare numero randomico
+        public int GenerateCode()
+        {
+            Random rnd = new Random();
+            return rnd.Next(10000000, 99999999);
+
+        }
+
+
+
+        public string GetProductString()
+        {
+            string rapprString = "Nome del prodotto: " + this.nameProduct + ", ";
+            rapprString += "Description: " + this.descriptionProduct + ", ";
+            rapprString += "Prezzo: " + this.price + ", ";
+            rapprString += "Barcode: " + this.barCode + ", ";
+            rapprString += "Is on sale? " + this.onSale + ", ";
+
+            return rapprString;
+        }
+
+
+    }
+}
---
+++ shop2/shop.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shop
+{
+    public class shop
+    {
+        //ATTRIBUTI
+        private string shopName;
+        private string shopCity;
+
+        private string shopAddress;
+        private int shopCivicNUmber;
+        private List<Product> ProductList;
+
+
+        //COSTRUTTORE
+        public shop(string shopName, string shopCity, string shopAddress, int shopCivicNUmber)
+        {
+
+            this.shopName = shopName;
+            this.shopCity = shopCity;
+            this.shopAddress = shopAddress;
+            this.ProductList = new List<Product>();
+            this.shopCivicNUmber = shopCivicNUmber;
+
+        }
+
+        //GETTERS setters
+        public  string ShopName
+        { get { return this.shopName; } }
+
+        public string ShopCity
+        { get { return this.shopCity; } set { this.shopCity = value; } }
+
+        public string ShopAddress
+        { get { return this.shopAddress; } set { this.shopAddress = value; } }
+
+        public int ShopCivicNUmber
+        {
+            get { return this.shopCivicNUmber; }
+            set { this.shopCivicNUmber = value; }
+        }
+
+        public List<Product> GetProducts()
+        {
+
+            return this.ProductList;
+
+
+        }
+
+        //metodi
+
+        public void AddProduct(Product newProduct)
+        {
+            ProductList.Add(newProduct);
+        }
+
+        public void RemoveProduct(Product removeProduct)
+        {
+
+            ProductList.Remove(removeProduct);
+        }
+
+
+        public void addProductList(List<Product> listAddProduct)
+        {
+
+
+            foreach (Product productAddnew in listAddProduct)
+            {
+                this.ProductList.Add(productAddnew);
+            }
+
+        }
+
+        public string strRappresetation()
+        {
+            string rapprString = "shop name: " + this.shopName + "\n";
+            rapprString += "shop city: " + this.shopCity + "\n";
+            rapprString += "shop address: " + this.shopAddress + "\n";
+            rapprString += "product: \n";
+
+            foreach (Product productScan in ProductList)
+            {
+                rapprString += " - " + productScan.GetProductString() + "\n";
+            }
+
+            rapprString += "\n";
+
+            return rapprString;
+        }
+
+
+
+
+    }
+}
